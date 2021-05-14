9fd0318fba03d79acee4687c6df2b0e60c74bd79
Woojo Kim <kwoojo0127@gmail.com>
2021-05-15 01:19:01 +0900

    Update index

--- index.ts
+++ index.ts
@@ -1,13 +1,33 @@
-import Discord from 'discord.js';
+import Discord, { Message } from 'discord.js';
 import dotenv from 'dotenv';
-
+import { prefix } from './config';
+import commands from './commands';
 dotenv.config();
-
 const { DISCORD_TOKEN } = process.env;
 
 const client = new Discord.Client();
-client.login(DISCORD_TOKEN);
 
 client.once('ready', () => {
-  console.log('ready');
+  console.log('Woozya bot is online');
 });
+
+client.on('message', (message: Message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  }
+
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift()?.toLowerCase();
+
+  if (!command) {
+    return;
+  }
+
+  if (command === '말') {
+    commands.ping(message);
+  } else if (command === 'help') {
+    commands.help(message);
+  }
+});
+
+client.login(DISCORD_TOKEN);
