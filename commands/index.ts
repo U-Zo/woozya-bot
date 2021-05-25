0c11e449e5315ea486500b8282c38d2ac904c47e
Woojo Kim <kwoojo0127@gmail.com>
2021-05-26 00:40:42 +0900

    Add stop feature for stop command
    Reset song queue and disconnect voice channel
    
    Resolves: #3

--- commands/index.ts
+++ commands/index.ts
@@ -2,12 +2,14 @@ import ping from './ping';
 import help from './help';
 import clean from './clean';
 import play from './play';
+import stop from './stop';
 
 const commands = {
   ping,
   help,
   clean,
   play,
+  stop,
 };
 
 export default commands;
