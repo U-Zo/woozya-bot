267ab1f7308f055185807e173b2522659bf6d972
Woojo Kim <kwoojo0127@gmail.com>
2021-07-11 00:26:23 +0900

    Add types for song and song queue
    Relates: #2

---
+++ types/songTypes.ts
@@ -0,0 +1,13 @@
+import { TextChannel, VoiceChannel, VoiceConnection } from 'discord.js';
+
+export interface Song {
+  title: string;
+  url: string;
+}
+
+export interface SongQueue {
+  voiceChannel: VoiceChannel;
+  textChannel: TextChannel;
+  connection: VoiceConnection;
+  songs: Song[];
+}
